17e5a2cb671c5858442105b53515d2c8349a544b
Evan Yamanishi <sh0ji@users.noreply.github.com>
2016-10-20 11:30:09 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+export default function() {
+    return {
+        'action': 'removeElement',
+        'selector': '[id^=_id],[class^=_id]'
+    }, {
+        'action': 'removeAttribute',
+        'attribute': 'lang',
+        'selector': '[lang]:not(html)'
+    }
+}
