1dcb47b71cd74641a5a1adedb1857080826b52d6
Evan Yamanishi <sh0ji@users.noreply.github.com>
2016-10-20 11:42:08 -0400

    should return an object

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 'use strict';
 
 export default function() {
-    return {
+    return [{
         'action': 'removeElement',
         'selector': '[id^=_id],[class^=_id]'
     }, {
         'action': 'removeAttribute',
         'attribute': 'lang',
         'selector': '[lang]:not(html)'
-    }
+    }]
 }
