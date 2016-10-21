a47f4e0b82838aa28d4b20ea2b28caa6300ecd4e
Evan Yamanishi <sh0ji@users.noreply.github.com>
2016-10-21 16:54:45 -0400

    just export the object. duh.

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 'use strict';
 
-exports.default = function() {
-    return [{
-        'action': 'removeElement',
-        'selector': '[id^=_id],[class^=_id]'
-    }, {
-        'action': 'removeAttribute',
-        'attribute': 'lang',
-        'selector': '[lang]:not(html)'
-    }]
-}
-module.exports = exports['default']
+module.exports = [{
+    'action': 'removeElement',
+    'selector': '[id^=_id],[class^=_id]'
+}, {
+    'action': 'removeAttribute',
+    'attribute': 'lang',
+    'selector': '[lang]:not(html)'
+}]
